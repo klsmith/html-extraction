22c50248a2501e8b4be38564c035fd93ddacebf2
Kooper Smith <klsmith6@bsu.edu>
2015-06-18 09:53:46 -0400

    Added test for getHTMLContentOfNthOccurance(tagName, n) method.
    While making the method pass testing, case where n was used have now
    been replaced with (n-1) because the nth occurance of an item in a list
    is at the index (n-1).

--- html-extraction/src/com/pkw/html/parsing/JSoupHtmlParser.java
+++ html-extraction/src/com/pkw/html/parsing/JSoupHtmlParser.java
@@ -6,34 +6,36 @@ import org.jsoup.select.Elements;
 
 public class JSoupHtmlParser implements HtmlParser {
 
-	Document document;
+    Document document;
 
-	public static JSoupHtmlParser createFor(String html) {
-		return new JSoupHtmlParser(html);
-	}
+    public static JSoupHtmlParser createFor(String html) {
+        return new JSoupHtmlParser(html);
+    }
 
-	public JSoupHtmlParser(String html) {
-		document = Jsoup.parse(html);
-		document.outputSettings().prettyPrint(false);
-	}
+    public JSoupHtmlParser(String html) {
+        document = Jsoup.parse(html);
+        document.outputSettings().prettyPrint(false);
+    }
 
-	@Override
-	public String getHTMLContentOfFirst(String tagName) {
-		Elements elements = document.getElementsByTag(tagName);
-		try {
-			return elements.get(0).html();
-		} catch (IndexOutOfBoundsException e) {
-			return "";
-		}
-	}
-	
-	public String getHTMLContentOfNthOccurance(String tagName, int n) {
-		Elements elements = document.getElementsByTag(tagName);
-		try {
-			return elements.get(n).html();
-		} catch (IndexOutOfBoundsException e) {
-			return "";
-		}
-	}
-	
+    @Override
+    public String getHTMLContentOfFirst(String tagName) {
+        Elements elements = document.getElementsByTag(tagName);
+        try {
+            return elements.get(0).html();
+        }
+        catch (IndexOutOfBoundsException e) {
+            return "";
+        }
+    }
+
+    public String getHTMLContentOfNthOccurance(String tagName, int n) {
+        Elements elements = document.getElementsByTag(tagName);
+        try {
+            return elements.get(n - 1).html();
+        }
+        catch (IndexOutOfBoundsException e) {
+            return "";
+        }
+    }
+
 }
--- html-extraction/test/com/pkw/html/parsing/JSoupHtmlParserTester.java
+++ html-extraction/test/com/pkw/html/parsing/JSoupHtmlParserTester.java
@@ -14,48 +14,54 @@ import com.pkw.html.parsing.JSoupHtmlParser;
 
 public class JSoupHtmlParserTester {
 
-	public static final String HTML = "<html><body><h1>example</h1><p>Welcome to example!</p><div id=\"firstdiv\"><h2>hello world</h2><div><p>I'm Example<br/>Batman</p></div></div></body></html>";
+    public static final String HTML = "<html><body><h1>example</h1><p>Welcome to example!</p><div id=\"firstdiv\"><h2>hello world</h2><div><p>I'm Example<br/>Batman</p></div></div></body></html>";
 
-	private JSoupHtmlParser parser;
+    private JSoupHtmlParser parser;
 
-	@Before
-	public void setup() throws ParserConfigurationException, SAXException,
-			IOException {
-		parser = JSoupHtmlParser.createFor(HTML);
-	}
+    @Before
+    public void setup() throws ParserConfigurationException, SAXException, IOException {
+        parser = JSoupHtmlParser.createFor(HTML);
+    }
 
-	@Test
-	public void testGetFirstDiv() {
-		String actual = parser.getHTMLContentOfFirst("div");
-		String expected = "<h2>hello world</h2><div><p>I'm Example<br>Batman</p></div>";
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetFirstDiv() {
+        String actual = parser.getHTMLContentOfFirst("div");
+        String expected = "<h2>hello world</h2><div><p>I'm Example<br>Batman</p></div>";
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testGetFirstH1Contents() {
-		String actual = parser.getHTMLContentOfFirst("h1");
-		String expected = "example";
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetFirstH1Contents() {
+        String actual = parser.getHTMLContentOfFirst("h1");
+        String expected = "example";
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testGetFirstPContents() {
-		String actual = parser.getHTMLContentOfFirst("p");
-		String expected = "Welcome to example!";
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetFirstPContents() {
+        String actual = parser.getHTMLContentOfFirst("p");
+        String expected = "Welcome to example!";
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testGetFirstBodyContents() {
-		String actual = parser.getHTMLContentOfFirst("body");
-		String expected = "<h1>example</h1><p>Welcome to example!</p><div id=\"firstdiv\"><h2>hello world</h2><div><p>I'm Example<br>Batman</p></div></div>";
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetFirstBodyContents() {
+        String actual = parser.getHTMLContentOfFirst("body");
+        String expected = "<h1>example</h1><p>Welcome to example!</p><div id=\"firstdiv\"><h2>hello world</h2><div><p>I'm Example<br>Batman</p></div></div>";
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testGetNonExistantTag() {
-		String actual = parser.getHTMLContentOfFirst("blah");
-		String expected = "";
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetNonExistantTag() {
+        String actual = parser.getHTMLContentOfFirst("blah");
+        String expected = "";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetSecondDivContents() {
+        String actual = parser.getHTMLContentOfNthOccurance("div", 2);
+        String expected = "<p>I'm Example<br>Batman</p>";
+        assertEquals(expected, actual);
+    }
 }
