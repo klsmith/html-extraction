ee12d52b3495b5e12c5d1e0864038b191bf618cf
Kooper Smith <klsmith6@bsu.edu>
2015-06-18 11:33:57 -0400

    Refactored Main to be more modular. Wes, feel free to revert this file if the changes are too drastic! Although, the functionality is exactly the same as before.

--- html-extraction/src/com/pkw/html/extraction/Main.java
+++ html-extraction/src/com/pkw/html/extraction/Main.java
@@ -9,26 +9,25 @@ import com.pkw.html.parsing.JSoupHtmlParser;
 public final class Main {
 
     private static final int NTH_OCCURANCE = 9;
+    private static final String SEARCH_FOR = "{searchFor}";
+    private static final String PRICE_CEILING = "{priceCeiling}";
+    private static final String EBAY_URL = "http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Auction=1&_sop=1&LH_FS=1&_nkw="
+            + SEARCH_FOR + "&_dcat=158671&rt=nc&_mPrRngCbx=1&_udlo&_udhi=" + PRICE_CEILING;
 
     private Main() {
         super();
     }
 
     public static void main(String[] args) {
-
-        String searchFor = "batman";
-
-        String priceCeiling = "0.25";
-
-        String html = getHTML("http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Auction=1&_sop=1&LH_FS=1&_nkw=" + searchFor
-                + "&_dcat=158671&rt=nc&_mPrRngCbx=1&_udlo&_udhi=" + priceCeiling);
-
-        JSoupHtmlParser test = JSoupHtmlParser.createFor(html);
-
-        System.out.println(JSoupHtmlParser//
-                .createFor(test.getHTMLContentOfNthOccurance("ul", NTH_OCCURANCE))//
+        JSoupHtmlParser parser = JSoupHtmlParser.createFor(getEbayHTML("batman", "0.25"));
+        System.out.println(JSoupHtmlParser.createFor(parser.getHTMLContentOfNthOccurance("ul", NTH_OCCURANCE))//
                 .getHTMLContentOfFirst("li"));
+    }
 
+    private static String getEbayHTML(String searchFor, String priceCeiling) {
+        return getHTML(EBAY_URL//
+                .replace(SEARCH_FOR, searchFor)//
+                .replace(PRICE_CEILING, priceCeiling));
     }
 
     public static String getHTML(String argUrl) {
